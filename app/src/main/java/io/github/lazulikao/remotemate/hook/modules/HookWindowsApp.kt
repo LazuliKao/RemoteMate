549da50539047ccdc8ca96d4c3b9bdfc69afdf65
LazuliKao <LazuliKao233@outlook.com>
2025-08-04 17:46:49 +0800

    add modules

--- app/src/main/java/io/github/lazulikao/remotemate/hook/modules/HookWindowsApp.kt
+++ app/src/main/java/io/github/lazulikao/remotemate/hook/modules/HookWindowsApp.kt
@@ -1,14 +1,7 @@
 package io.github.lazulikao.remotemate.hook.modules
 
-import com.highcapable.kavaref.KavaRef
 import com.highcapable.kavaref.KavaRef.Companion.resolve
-import com.highcapable.kavaref.condition.base.MemberCondition
 import com.highcapable.yukihookapi.hook.param.PackageParam
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
-import link.lk233.oneuiceiler.hook.IHookModule
-import kotlin.apply
-import kotlin.collections.joinToString
 
 
 class HookWindowsApp : IHookModule {
@@ -18,35 +11,35 @@ class HookWindowsApp : IHookModule {
     override fun onLoad(env: PackageParam) {
 
         env.loadApp("com.microsoft.rdc.androidx") {
-            "com.microsoft.a3rdc.rdp.NativeRdpConnection".toClassOrNull()?.resolve()?.apply {
-                firstMethodOrNull {
-                    name = "sendScanCodeKey"
-                }?.hook {
-                    after {
-                        if (enableLog) {
-                            logDebug("HookWindowsApp: sendScanCodeKey called with args: ${args.joinToString()}")
-                        }
-                    }
-                }
-                firstMethodOrNull {
-                    name = "sendSmartKey"
-                }?.hook {
-                    after {
-                        if (enableLog) {
-                            logDebug("HookWindowsApp: sendSmartKey called with args: ${args.joinToString()}")
-                        }
-                    }
-                }
-                firstMethodOrNull {
-                    name = "sendVirtualKey"
-                }?.hook {
-                    after {
-                        if (enableLog) {
-                            logDebug("HookWindowsApp: sendVirtualKey called with args: ${args.joinToString()}")
-                        }
-                    }
-                }
-            }
+//            "com.microsoft.a3rdc.rdp.NativeRdpConnection".toClassOrNull()?.resolve()?.apply {
+//                firstMethodOrNull {
+//                    name = "sendScanCodeKey"
+//                }?.hook {
+//                    after {
+//                        if (enableLog) {
+//                            logDebug("HookWindowsApp: sendScanCodeKey called with args: ${args.joinToString()}")
+//                        }
+//                    }
+//                }
+//                firstMethodOrNull {
+//                    name = "sendSmartKey"
+//                }?.hook {
+//                    after {
+//                        if (enableLog) {
+//                            logDebug("HookWindowsApp: sendSmartKey called with args: ${args.joinToString()}")
+//                        }
+//                    }
+//                }
+//                firstMethodOrNull {
+//                    name = "sendVirtualKey"
+//                }?.hook {
+//                    after {
+//                        if (enableLog) {
+//                            logDebug("HookWindowsApp: sendVirtualKey called with args: ${args.joinToString()}")
+//                        }
+//                    }
+//                }
+//            }
             "android.view.inputmethod.InputMethodManager".toClassOrNull()?.resolve()?.apply {
                 firstMethodOrNull {
                     name = "showSoftInput"
@@ -72,6 +65,30 @@ class HookWindowsApp : IHookModule {
                             result = true
                         }
                     }
+            } ?: run {
+                "android.content.SharedPreferences".toClassOrNull()?.resolve()?.apply {
+                    firstMethodOrNull {
+                        name = "getBoolean"
+                        parameters(String::class.java, Boolean::class.java)
+                    }?.hook {
+                        after {
+                            val key = args[0] as String
+                            val defaultValue = args[1] as Boolean
+                            val resultBool = result as Boolean
+                            if (enableLog) {
+                                logDebug("HookWindowsApp: getBoolean called with key: $key, defaultValue: $defaultValue, result: $resultBool")
+                            }
+                            // If the key is "scancode_enabled", return true to enable scancode
+                            if (key == "scancode_enabled_v2") {
+                                result = true
+                                if (enableLog) {
+                                    logDebug("HookWindowsApp: Overriding getBoolean for key 'scancode_enabled' to return true")
+                                }
+                            }
+                        }
+                    }
+                }
+
             }
         }
     }
